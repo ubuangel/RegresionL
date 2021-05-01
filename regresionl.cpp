79e4bbb5901c76862df7ed809ef5bf056151214f
angel <andresbjar97@gmail.com>
2021-05-01 17:41:59 -0500

    paso1

--- regresionl.cpp
+++ regresionl.cpp
@@ -1,3 +1,37 @@
 #include<iostream>
+using namespace std;
 //regresion 
+
 cout<<"regresion lineal modificar parametros y hiper parametros"
+
+
+//como clacular los los puntos 
+//calculo de la hipotesis 
+
+void func(int w0,int w1,int x){
+
+	return w0+w1*x;
+//calculo de error
+
+
+//calculo de las derivadas
+
+//calculo de la gradiente 
+
+	
+	int main(){
+	
+		for (int i=0;i<6;i++)
+	func(1,2,3);
+	}
+//
+
+
+
+
+
+
+
+
+
+
