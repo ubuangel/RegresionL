13df9a09e432ad983319495dcaf25b33e454b126
angel <andresbjar97@gmail.com>
2021-05-01 20:47:33 -0500

    mm

--- regresionl.cpp
+++ regresionl.cpp
@@ -6,25 +6,28 @@ using namespace std;
 //regresion 
 
 
-
-
 //como clacular los los puntos 
 //calculo de la hipotesis 
 
-int hx(int w0,int w1,int x){
-	
+int hx(int w0,int w1){
+	int f;
+	int tam=6;
+	for (int i=0;i<tam;i++){
 		
-	return w0+w1*x;
+	 f=w0+w1*i;
+	
+	}
+	return f;
 	
 	}
 
 
 //calculo de error
-void error(int n,int prediccion[]){
+void error(int n,int x[],int w0,int w1,int prediccion[]){
 	int err,suma;
 	
 	for (int iter=0;iter<n;iter++){
-		suma=suma+(prediccion[iter]-hx(prediccion[iter],a,b);
+		suma=suma+(prediccion[iter]-hx(w0,w1));
 	}
 	 
 	for(int iter=0;iter<n;iter++){
@@ -39,23 +42,24 @@ void error(int n,int prediccion[]){
 //(1/n)*sumatoria(yi-hx)(-1) derivada respecto a w0
 //(1/n)*sumatoria(yi-hx)(-x) derivada respecto a w1
 
-void gradiente(int  learning,int epocas,int prediccion[]){
+void gradiente(int n,int  learning,int epocas,int prediccion[],int w0,int w1){
 
 int i;
+	int teta;
 while(i<epocas){
-	int teta=0;
 	int sumatoria=0;
 	int derivada;
 	for (int t=0;t<6;t++){
 	
-	sumatoria=sumatoria+(prediccion[i]-hx(a,b,c))
+	sumatoria=sumatoria+(prediccion[t]-hx(w0,w1));
 	derivada=(1/n)*sumatoria*(-1);
 	teta=teta+learning*derivada;
 	
 	
-	}
+		}
 	i++;	
 	}
+	cout<<teta;
 		}
 
 //calculo de la gradiente 
@@ -75,13 +79,15 @@ while(i<epocas){
 		 b=rand()%(10-1)+1;
 		 x=rand()%(10-1)+1;
 		 
-		prediccion[i]= hx(a,b,x);
+		//prediccion[i]= hx(a,b,x);
 		 }
 	for (int i=0;i<6;i++){
-		
-		cout<<prediccion[i]<<" ";
-		
+		prediccion[i]=i;
+		//cout<<prediccion[i]<<" ";
+//		
 		}
+		gradiente(2,0.3,6,prediccion,2,3);
+
 	
 	
 	}
