95d32308cb6918a93e235676e0abfe040864e9e7
angel <andresbjar97@gmail.com>
2021-05-05 10:08:45 -0500

    imprimir valores

--- Relineal.py
+++ Relineal.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x_ds=[i for i in range (100)]
-y_ds=[i*np.random.normal(0,5) for i in x_ds]	
+y_ds=[i+np.random.normal(0,5) for i in x_ds]	
 
 
 
@@ -12,9 +12,11 @@ def h(x,w):
 	
 w=np.random.rand(2)
 print(w)
-y_prediccion=[h(1,w) for i in x_ds]
+y_prediccion=[h(i,w) for i in x_ds]
 
-plt.plot(x_ds,y_ds,'*')
-plt.plot(x_ds,y_prediccion)
+
 #for i in x_ds:
-#	y_p=h(i,w)#voy aplicar una funcion h a cada elemento i pero cada elemto i esta en el conjunto x_ds
+#	y_prediccion=h(i,w) #voy aplicar una funcion h a cada elemento i pero cada elemto i esta en el conjunto x_ds
+plt.plot(x_ds,y_ds,'*')#nube de puntos
+plt.plot(x_ds,y_prediccion)#prediccion
+
