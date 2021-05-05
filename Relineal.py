8f8b4d81fb5b395492a47bb5f7f42a3cca7f6e14
angel <andresbjar97@gmail.com>
2021-05-05 13:23:48 -0500

    puliendo

--- Relineal.py
+++ Relineal.py
@@ -5,14 +5,12 @@ x_ds=[i for i in range (20)]
 y_ds=[i+np.random.normal(0,5) for i in x_ds]	
 
 
-
-
 def h(x,w):
 	return w[0]+w[1]*x
 	
-w=np.random.rand(2)
+
 #print(w)
-y_prediccion=[h(i,w) for i in x_ds]
+
 
 
 #for i in x_ds:
@@ -24,14 +22,20 @@ y_prediccion=[h(i,w) for i in x_ds]
 #mse
 #mse
 #manera de lsitas pro compresion
+
 def error(y,x,w):
 	return sum( [ (e[0] - h(e[1],w))**2 for  e in zip(y,x) ])/(2*len(y))
+
+w=np.random.rand(2)
+
+y_prediccion=[h(i,w) for i in x_ds]
+
 	
-print(error)
+print("error mse"+str( error(y_ds,x_ds,w)))
 
 
-#plt.plot(x_ds,y_ds,'*')#nube de puntos
-#plt.plot(x_ds,y_prediccion)#prediccion
+plt.plot(x_ds,y_ds,'*')#nube de puntos
+plt.plot(x_ds,y_prediccion)#prediccion
 #otra forma
 #def error(x,y,w):
 #for i in range(len(y))
@@ -39,23 +43,23 @@ print(error)
 #	return suma/2*len(y)
 	
 #calcular las derivadas
-def grad(y,x,w):
-	gra_w0= sum ([(e[0] -h(e[1],w))*(-1) for e in zip(y,x)])/len(y)
-	gra_w1= sum ([(e[0] -h(e[1],w))*(-e[1]) for e in zip(y,x)])/len(y)
-	return gra_w0,gra_w1
-	
-#creamos un algoritmo de aprendisaje
+#def grad(y,x,w):
+#	gra_w0= sum ([(e[0] -h(e[1],w))*(-1) for e in zip(y,x)])/len(y)
+#	gra_w1= sum ([(e[0] -h(e[1],w))*(-e[1]) for e in zip(y,x)])/len(y)
+#	return gra_w0,gra_w1
+#	
+##creamos un algoritmo de aprendisaje
 
 
-def train(x_ds,y_ds,w,epochs,alpha):
-	for i in range(epochs):
-		err=(y_ds,x_ds,w)
-		print(err)
-		grad_w0=grad(y_ds,x_ds,w)
-		grad_w1=grad(y_ds,x_ds,w)
-	
-		w[0]=w[0]-alpha*grad_w0
-		w[1]=w[1]-alpha*grad_w1
-	
+#def train(x_ds,y_ds,w,epochs,alpha):
+#	for i in range(epochs):
+#		err=(y_ds,x_ds,w)
+#		#print(err)
+#		grad_w0=grad(y_ds,x_ds,w)
+#		grad_w1=grad(y_ds,x_ds,w)
+#	
+#		w[0]=w[0]-alpha*grad_w0
+#		w[1]=w[1]-alpha*grad_w1
+#	
 
-train(x_ds,y_ds,w,100,0.0001)
+#train(x_ds,y_ds,w,100,0.0001)
