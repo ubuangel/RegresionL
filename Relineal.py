8a559f0930a41ebf82f1c43dd9613f56b29494ce
angel <andresbjar97@gmail.com>
2021-05-05 12:15:02 -0500

    algoritmo de aprendizaje

--- Relineal.py
+++ Relineal.py
@@ -17,6 +17,27 @@ y_prediccion=[h(i,w) for i in x_ds]
 
 #for i in x_ds:
 #	y_prediccion=h(i,w) #voy aplicar una funcion h a cada elemento i pero cada elemto i esta en el conjunto x_ds
+#plt.plot(x_ds,y_ds,'*')#nube de puntos
+#plt.plot(x_ds,y_prediccion)#prediccion
+
+
+#mse
+#mse
+
+def error(y,x,w):
+	return sum( [ (e[0] - h(e[1],w))**2 for  e in zip(y,x) ])/(2*len(y))
+	
+print(error)
+
+
 plt.plot(x_ds,y_ds,'*')#nube de puntos
 plt.plot(x_ds,y_prediccion)#prediccion
+#otra forma
+#def error(x,y,w):
+#for i in range(len(y))
+#	suma=suma+(y[i]-h(x[i],w))**2
+#	return suma/2*len(y)
+	
+#creamos un algoritmo de aprendisaje
+
 
